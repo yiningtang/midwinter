49743620a59abe9979bf33202b62db6e9e453662
yiningtang <yiningcherry@gmail.com>
2016-03-18 21:10:05 +1100

    updated app.js

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,36 +8,29 @@
  *
  * Main module of the application.
  */
- var app=angular
- .module('portfolio1App', [
-  'ui.router'
-  ]);
+var app = angular
+    .module('portfolio1App', [
+        'ui.router'
+    ]);
 
- app.config(function($stateProvider, $urlRouterProvider) {
-  $urlRouterProvider.otherwise("/home");
-  //
-  // Now set up the states
-  $stateProvider
-  .state('home', {
-    url: "/home",
-    templateUrl: "views/home.html",
-    activetab: 'home'
-  })
-  .state('about', {
-    url: "/about",
-    templateUrl: "views/about.html",
-     activetab: 'about'
-  })
-  .state('contact', {
-    url: "/contact",
-    templateUrl: "views/contact.html", 
-    activetab: 'contact'
-  });
+app.config(function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise("/home");
+    //
+    // Now set up the states
+    $stateProvider
+        .state('home', {
+            url: "/home",
+            templateUrl: "views/home.html",
+            activetab: 'home'
+        })
+        .state('about', {
+            url: "/about",
+            templateUrl: "views/about.html",
+            activetab: 'about'
+        })
+        .state('contact', {
+            url: "/contact",
+            templateUrl: "views/contact.html",
+            activetab: 'contact'
+        });
 });
-  app.run(function($rootScope) {
- 
-   /**$rootScope.clicked = false;
-   $rootScope.click=function(){
-     $rootScope.clicked=true;
-   };**/
- });
